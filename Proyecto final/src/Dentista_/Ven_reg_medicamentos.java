8cdba9b265089e80be49dd7ec05c0e677f6be539
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-05-27 10:17:23 -0400

    Listo para partir

---
+++ Proyecto final/src/Dentista_/Ven_reg_medicamentos.java
@@ -0,0 +1,87 @@
+package Dentista_;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class Ven_reg_medicamentos extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ven_reg_medicamentos frame = new Ven_reg_medicamentos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Ven_reg_medicamentos() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 618, 405);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(128, 0, 128));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nombre del medicamento");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setBounds(160, 104, 198, 13);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("Registro de medicamentos ");
+		lblNewLabel.setBounds(169, 10, 315, 32);
+		lblNewLabel.setBackground(new Color(255, 255, 255));
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(160, 127, 214, 39);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Fecha de caducidad");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setBounds(160, 192, 198, 13);
+		contentPane.add(lblNewLabel_2);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(160, 232, 214, 39);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btn_reg_med = new JButton("Registrar");
+		btn_reg_med.setBounds(160, 305, 101, 21);
+		contentPane.add(btn_reg_med);
+		
+		JButton btn_cancelar = new JButton("Cancelar");
+		btn_cancelar.setBounds(290, 305, 85, 21);
+		contentPane.add(btn_cancelar);
+	}
+}
